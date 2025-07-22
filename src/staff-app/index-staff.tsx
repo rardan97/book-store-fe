ff6b7df69f0811bdc1cda2bb63a1a9ef773e6591
rardan97 <risalardan@gmail.com>
2025-07-22 09:57:24 +0700

    update github action

--- src/staff-app/index-staff.tsx
+++ src/staff-app/index-staff.tsx
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import AppStaff from './AppStaff';
 import { ThemeProvider } from './context/ThemeContext';
 import { AppWrapper } from './components/common/PageMeta';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
+<StrictMode>      
+    <BrowserRouter>
+    
+    
 <ThemeProvider>
     <AppWrapper>
         <AppStaff />
     </AppWrapper>
 </ThemeProvider>
+</BrowserRouter>
+</StrictMode>
 );
--- src/user-app/index-user.tsx
+++ src/user-app/index-user.tsx
@@ -1,9 +1,17 @@
 
 import ReactDOM from 'react-dom/client';
 import AppUser from './AppUser';
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-        <AppUser />
+        <StrictMode>
+    <BrowserRouter>
+     <AppUser />
+    </BrowserRouter>
+       
+
+        </StrictMode>
 );
