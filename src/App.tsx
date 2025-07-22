df71dfd0c37e14bae9171baa985bd8a89c1af2c3
rardan97 <risalardan@gmail.com>
2025-07-22 08:48:54 +0700

    update github action

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
+
 import AppStaff from "./staff-app/AppStaff";
 import AppUser from "./user-app/AppUser";
 
 export default function App() {
 
-  return (
-    <>
-    <Router>
-<Routes>
-          <Route path="/staff/*" element={<AppStaff />} />
-          <Route path="/*" element={<AppUser />} />
-          {/* <Route path="/" element={<HomePage />}/> */}
-        </Routes>    
-    </Router>
+  // return (
+    const routes = useRoutes([
+      { path: "/staff", element: <AppStaff /> },
+    { path: "/*", element: <AppUser /> },
+    // { path: "/", element: <HomePage /> }
+  ]);
+
+  return routes;
+    // <>
+
+    //   <Routes>
+    //             <Route path="/staff/*" element={<AppStaff />} />
+    //             <Route path="/*" element={<AppUser />} />
+    //             {/* <Route path="/" element={<HomePage />}/> */}
+    //           </Routes>    
+         
         
-    </>
-  );
+    // </>
+  // );
 }
